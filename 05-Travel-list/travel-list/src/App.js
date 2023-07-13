8d4fdf0d8305b9e052bf163a30da9ba901f9d6a7
Gaurav <gauravpatilfamily7@gmail.com>
2023-07-13 20:21:36 +0530

    Rendering the Item list

--- 05-Travel-list/travel-list/src/App.js
+++ 05-Travel-list/travel-list/src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Mobile ", quantity: 2, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -22,7 +28,28 @@ function Form() {
 }
 
 function ItemList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+          // <Item item ={item}  1st is name of component , 2nd  is name of prop and 3rd is object itself
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Status() {
